92079303c8839c171e6ab8bdaedf267fc3670a35
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-08-01 11:10:19 +0530

    3 problems added

---
+++ Medium/DepartmentHighestSalary.sql
@@ -0,0 +1,40 @@
+-- Table: Employee
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | id           | int     |
+-- | name         | varchar |
+-- | salary       | int     |
+-- | departmentId | int     |
+-- +--------------+---------+
+
+-- id is the primary key (column with unique values) for this table. departmentId is a foreign key (reference columns) of the ID from the Department table. Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+ 
+
+-- Table: Department
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+
+-- id is the primary key (column with unique values) for this table. It is guaranteed that department name is not NULL. Each row of this table indicates the ID of a department and its name.
+ 
+
+-- Write a solution to find employees who have the highest salary in each of the departments. Return the result table in any order.
+
+
+
+
+
+SELECT D.name AS Department,E.name AS Employee,E.salary AS Salary 
+FROM Department D
+LEFT JOIN Employee E
+ON D.id = E.departmentId
+WHERE (departmentId,salary) IN 
+(SELECT departmentId,MAX(salary)
+FROM Employee
+GROUP BY departmentId);
